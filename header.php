dfa3c56058bec8c4e0ff570a513d4ee738fcc0d4
Manish <107940936+Manish7194@users.noreply.github.com>
2024-04-12 12:02:51 +0530

    Add files via upload
    Its a tech hub website using html css js and php

---
+++ header.php
@@ -0,0 +1,40 @@
+<style>     
+    .navbar-fixed-top{
+        margin-bottom: 0px;
+        position: fixed;
+        top: 0;
+        width: 100%;
+    }
+</style>
+<nav class="navabar-fixed-top navbar-inverse">
+               <div class="container">
+                   <div class="navbar-header">
+                       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                           <span class="icon-bar"></span>
+                           <span class="icon-bar"></span>
+                           <span class="icon-bar"></span>
+                       </button>
+                       <a href="index.php" class="navbar-brand"><img src="img/logo.jpg" height=30px alt=""></a>
+                   </div>
+                   
+                   <div class="collapse navbar-collapse" id="myNavbar">
+                       <ul class="nav navbar-nav navbar-right">
+                           <?php
+                           if(isset($_SESSION['email'])){
+                           ?>
+                           <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
+                           <li><a href="settings.php"><span class="glyphicon glyphicon-cog"></span> Settings</a></li>
+                           <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+                           <?php
+                           }else{
+                            ?>
+                            <li><a href="signup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                           <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                           <?php
+                           }
+                           ?>
+                           
+                       </ul>
+                   </div>
+               </div>
+</nav>
---
+++ header2.php
@@ -0,0 +1,28 @@
+<style>     
+    .navbar-fixed-top{
+        margin-bottom: 0px;
+        position: fixed;
+        top: 0;
+        width: 100%;
+    }
+</style>
+<nav class="navabar-fixed-top navbar-inverse">
+               <div class="container">
+                   <div class="navbar-header">
+                       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                           <span class="icon-bar"></span>
+                           <span class="icon-bar"></span>
+                           <span class="icon-bar"></span>
+                       </button>
+                       <a href="index.php" class="navbar-brand">TechHub</a>
+                   </div>
+                   
+                   <div class="collapse navbar-collapse" id="myNavbar">
+                       <ul class="nav navbar-nav navbar-right">
+                           <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
+                           <li><a href="settings.php"><span class="glyphicon glyphicon-cog"></span> Settings</a></li>
+                           <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+                       </ul>
+                   </div>
+               </div>
+</nav>
